aebaacd539251eac039f1c262a5cf81baa07f184
George Florea Bănuș <georgefb899@gmail.com>
2025-07-18 18:24:03 +0300

    main: setup KLocalizedContext

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,23 @@
 #include <QQmlApplicationEngine>
 #include <QtWebEngineQuick>
 
+#include <KLocalizedString>
+
+using namespace Qt::StringLiterals;
+
 int main(int argc, char *argv[])
 {
     QtWebEngineQuick::initialize();
 
     QGuiApplication app(argc, argv);
+    QGuiApplication::setWindowIcon(QIcon::fromTheme(u"rina"_s));
+    KLocalizedString::setApplicationDomain("rina");
 
     QQmlApplicationEngine engine;
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
                      &app, []() { QCoreApplication::exit(-1); },
     Qt::QueuedConnection);
+    engine.rootContext()->setContextObject(new KLocalizedContext(&app));
     engine.loadFromModule("com.georgefb.rina", "Main");
 
     return app.exec();
